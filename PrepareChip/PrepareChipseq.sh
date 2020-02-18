b97c6a3274690630273879f3beb8dc9f32e44491
Caleb Kibet <ckibet@icipe.org>
2020-02-18 15:23:25 +0300

    document

--- PrepareChip/PrepareChipseq.sh
+++ PrepareChip/PrepareChipseq.sh
@@ -1,7 +1,7 @@
 #Above parameters can be changed to pick a different file
 #Might need to make this into a python script and make it as generic as possible
 function Getfasta {
-				cut -f 1,2,3 $input_BED | bed-widen -width $siz >$cl-$tf-$siz.bed
+				cut -f 1,2,3 $inputPEAK | bed-widen -width $siz >$cl-$tf-$siz.bed
 				if [ -s "$cl-$tf-$siz.bed" ]; then
 				
 					#Extract the negative sequence
@@ -10,7 +10,7 @@ function Getfasta {
 					fastaFromBed -tab -fi $hg -bed $cl-$tf-$siz.negbed -fo $cl-$tf-$siz.negfa
 					
 					# Prepare the fasta seqences 
-					cut -f 7 $input_BED >/tmp/f1
+					cut -f 7 $inputPEAK >/tmp/f1
 					cut -f 1 $cl-$tf-$siz.fa >/tmp/f2
 					cut -f 2 $cl-$tf-$siz.fa >/tmp/f3
 					paste /tmp/f2 /tmp/f1 /tmp/f3  >/tmp/$cl-$tf-$siz.fasta
@@ -18,7 +18,7 @@ function Getfasta {
 					rm /tmp/f*
 
 					#Do the same for the negative sequences
-					cut -f 7 $input_BED >/tmp/f1
+					cut -f 7 $inputPEAK >/tmp/f1
 					cut -f 1 $cl-$tf-$siz.negfa >/tmp/f2
 					cut -f 2 $cl-$tf-$siz.negfa >/tmp/f3
 					paste /tmp/f2 /tmp/f1 /tmp/f3  >/tmp/$cl-$tf-$siz.negfasta
@@ -36,10 +36,9 @@ function Getfasta {
 	}
 
 hg=$1 #$HOME/Project/Data/genomes/hg19/hg19.fa #change this to your hg path
-lab=$2 #Sy
+inputPEAK=$2
 cl=$3 #Hepg2
 tf=$4 #Max
 siz=$5 #100 #the length of the motifs
+lab=$6 #lab or the source of the ChIP-seq data
 Getfasta
-
-#python Assess_motifs.py Max-jas.meme $lab-$cl-$tf-$siz.posneg energyscore $lab-$cl-$tf-$siz
---
+++ PrepareChip/README.md
@@ -0,0 +1,11 @@
+## PrepareChip
+
+Script for extracting posneg file used by MARS. 
+
+PrepareChipseq.sh preproceses the peak files for motif evaluation.
+
+Requires:
+    - BedTools
+    - bed-widen script
+    - extractnegative.py
+    -removemasked.py
