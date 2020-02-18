a70880b40c22301c976f9ca31f40fc1a4b891976
Caleb Kibet <ckibet@icipe.org>
2020-02-18 14:04:48 +0300

    Remove MARSTools Backup

--- MARSTools_back_ups/README.txt
+++
@@ -1,6 +0,0 @@
-13th December 2016
-This folder contains backup of Old scripts previously used by MARSTools.
- Kept here for reference. Some functions here were also used in some of
- the data processing, therefore may be needed latter.
- 
-
--- MARSTools_back_ups/run_gimme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-for tf in Egr1 #$(ls /home/kipkurui/Project/Motif_Assessment/GIMME_MAP/)
-    do
-        cd /home/kipkurui/Project/Motif_Assessment/GIMME_MAP/"$tf"
-        echo "$tf"
-        gimme roc "$tf"_best.pwm "$tf"_test.fa "$tf"_bg.fa >"$tf"_roc.txt
-    done
